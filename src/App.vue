645947e1d28ac4816fdc39fb87cb602ca4aef769
Alexander Chernyh <alexchernyh2000@mail.ru>
2024-01-30 16:19:04 +0300

    add components

--- src/App.vue
+++ src/App.vue
@@ -1,41 +1,40 @@
 <template>
   <div class="app">
-    <div>
-      <form>
-        <h4>Создание поста</h4>
-        <input class="input" type="text" placeholder="Название">
-        <input class="input" type="text" placeholder="Описание">
-        <button class="btn" @click="createPost">Создать</button>
-      </form>
-    </div>
-    <div class="post" v-for="post in posts" v-bind:key="post">
-      <div><strong>Название:</strong>{{ post.title }}</div>
-      <div><strong>Описание:</strong>{{ post.body }}</div>
-    </div>
+    <post-form/>
+    <post-list :posts="posts"/>
   </div>
-
 </template>
 
 <script>
-
+import PostForm from "./components/PostForm"
+import PostList from "@/components/PostList"
 export default {
+  components: {
+    PostForm, PostList
+  },
   data() {
     return {
       posts: [
-        {id: 1, title:'JavaScript', body:'Описание поста'},
-        {id: 2, title:'JavaScript 2', body:'Описание поста 2'},
-        {id: 3, title:'JavaScript 3', body:'Описание поста 3'},
-        {id: 4, title:'JavaScript 4', body:'Описание поста 4'},
-      ]
+        {id: 1, title:' JavaScript', body:' Описание поста'},
+        {id: 2, title:' JavaScript 2', body:' Описание поста 2'},
+        {id: 3, title:' JavaScript 3', body:' Описание поста 3'},
+        {id: 4, title:' JavaScript 4', body:' Описание поста 4'},
+      ],
+      title: '',
+      body: '',
     }
   },
   methods: {
-    addLike() {
-      this.likes +=1;
+    createPost() {
+      const newPost = {
+        id: '',
+        title: this.title,
+        body: this.body,
+      }
+      this.posts.push(newPost);
+      this.title = '';
+      this.body = '';
     },
-    addDislike() {
-      this.dislikes +=1;
-    }
   }
 }
 </script>
@@ -46,30 +45,7 @@ export default {
   padding: 0;
   box-sizing: border-box;
 }
-.post {
-  padding: 15px;
-  border: 2px solid teal;
-  margin-top: 15px;
-}
-form {
-  display: flex;
-  flex-direction: column;
-}
 .app {
   padding: 20px;
-}
-.input {
-  width: 100%;
-  border: 1px solid teal;
-  padding: 10px 15px;
-  margin-top: 15px;
-}
-.btn {
-  margin-top: 15px;
-  align-self: flex-end;
-  padding: 10px;
-  background: none;
-  color: teal;
-  border: 1px solid teal;
 }
 </style>
