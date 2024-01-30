046b7a396ea5a40b91543f2923c29eda005eb9bc
Alexander Chernyh <alexchernyh2000@mail.ru>
2024-01-30 15:35:43 +0300

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <button @click="addLike">Like</button>
+    <button @click="addDislike">Dislike</button>
+    Работает!
+    <div>Количество лайков: <strong>{{ likes }}</strong></div>
+    <div>Количество дислайков: <strong>{{ dislikes }}</strong></div>
+  </div>
+
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      likes: 0,
+      dislikes: 0,
+    }
+  },
+  methods: {
+    addLike() {
+      this.likes +=1;
+    },
+    addDislike() {
+      this.dislikes +=1;
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
